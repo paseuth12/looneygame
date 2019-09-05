ba2e343670904a3914670a5358af1930b987ff65
paseuth <paseuth_phongsavanh@hotmail.com>
2019-09-05 12:23:12 +0700

    add a Full game

--- Node2D.gd
+++ Node2D.gd
@@ -1,21 +1,52 @@
 extends Control
 
-var story = "hello my name is %s My last name is %s What is your pet %s What do you like to eat"
-var answers = ["Ping","Pp","Cat","meet"]
+var story = "Your Answer is %s  The DragonSlyer is %s Your weapon is %s Defens weapon is %s"
+var answers = []
 var questions = []
+var questions_number = 0
 
 func _ready():
-	print (story && answers)
-	$Displaytext.text = "welcome!!! \nWhat is your name?"
+	$Displaytext.text = "Do you want to kill the dragon?"
 	
-	questions.append("welcome!!! \nWaht is your name?")
-	questions.append("What is your last name?")
-	questions.append("what is yor pet?")
-	questions.append("What do you like to eat?")
+	questions.append("You need to protect our village!!! \n Yes or No?")
+	questions.append("Do you want to kill the Dragon and become a DragonSlyer put down \nYour name?")
+	questions.append("Fist thing You must chooes the weapon \nSword or Arrow?")
+	questions.append("Next you nees a Defend weapon \nArmor or shield")
+	
+	
+	
+	$Displaytext.text = questions[0]
+	
 	print(questions)	
 	print(len(questions))
 	print(questions[0])
 	print(questions[3])
 
 func _on_PlayerButton_pressed():
-	$Displaytext.text = "PING"
+	if questions_number == 0:
+		# answer the question
+		answers.append($PlayerInput.text)
+		print( answers )
+		$Displaytext.text = questions[1]
+		$PlayerInput.text = ""
+	
+	if questions_number == 1:
+		# answer the question
+		answers.append($PlayerInput.text)
+		print( answers )
+		$Displaytext.text = questions[2]
+		$PlayerInput.text = ""
+	if questions_number == 2:
+		# answer the question
+		answers.append($PlayerInput.text)
+		print( answers )
+		$Displaytext.text = questions[3]
+		$PlayerInput.text = ""
+	if questions_number == 3:
+		# answer the question
+		answers.append($PlayerInput.text)
+		print( answers )
+		$Displaytext.text = story % answers
+		$PlayerInput.text = ""
+
+	questions_number = questions_number +1
